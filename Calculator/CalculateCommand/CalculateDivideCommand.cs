e14f4bb926f731dc3a1d98ce9dd506928aa1c8b3
ss.savenko <ss.savenko@gmail.com>
2018-08-11 10:50:14 +0300

    Close #5

--- Calculator/CalculateCommand/CalculateDivideCommand.cs
+++ Calculator/CalculateCommand/CalculateDivideCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calculator.CalculateCommand
+﻿namespace Calculator.CalculateCommand
 {
     internal class CalculateDivideCommand : ICalculateCommand
     {
--- Calculator/CalculatorForm.cs
+++ Calculator/CalculatorForm.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using Calculator.CalculateCommand;
+﻿using Calculator.CalculateCommand;
+using System;
 using System.Windows.Forms;
 
 namespace Calculator
