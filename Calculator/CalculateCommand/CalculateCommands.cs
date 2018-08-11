0cfed62c6d43bab1d8a73dbe7fdd341f512af2ba
ss.savenko <ss.savenko@gmail.com>
2018-08-11 10:57:59 +0300

    Close #7 #8

--- Calculator/CalculateCommand/CalculateCommands.cs
+++ Calculator/CalculateCommand/CalculateCommands.cs
@@ -18,7 +18,7 @@
         public int PeformCommands(int value1, int value2, char commandCharacter)
         {
             int returnValue = 0;
-            for(int i =0; i< commands.Length;i++)
+            for(int i = 0; i < commands.Length;i++)
             {
                 returnValue = commands[i].Calculate(value1, value2, commandCharacter);
                 if (returnValue != 0)
